7917bdf356c7605c811d287749509b62fbef9a88
ivonnenabangala <ivonnenabangala@gmail.com>
2025-02-17 02:09:17 +0300

    work on sending sms after parcel has been created

--- sms/sendSMS.js
+++ sms/sendSMS.js
@@ -3,7 +3,7 @@ const router = express.Router();
 require('dotenv').config();
 
 const credentials = {
-    apiKey: 'atsk_435bff255cbae11780d1945ea99fcbbff1712856a62cf8afedfef94f064ac4fdbcb78b7b', 
+    apiKey: process.env.AT_API_KEY, 
     username: 'senditparcel'  
 };
 
